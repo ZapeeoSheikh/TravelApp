466ea8be1dc371e85d14142023e2016ab448adc6
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-18 02:45:03 +0500

    Day 2 Completed

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelapp/pages/home_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -8,12 +9,20 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  List pages = [
+    HomePage(),
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.black54.withOpacity(0.3),
+        elevation: 0,
         items: [
-          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home_filled)),
           BottomNavigationBarItem(
               label: "Bars", icon: Icon(Icons.bar_chart_sharp)),
           BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
