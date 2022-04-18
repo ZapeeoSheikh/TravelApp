7cda88b91187e8896c69b312e8b2f65b37f7298f
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-19 01:38:31 +0500

    NavigationBar Comp ==

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
     return Container(
       margin: EdgeInsets.only(top: 80, left: 20, right: 15),
       child: Column(
@@ -37,16 +43,26 @@ class HomePage extends StatelessWidget {
               Expanded(child: Container()),
             ],
           ),
-          TabBar(tabs: [
-            Text("Places"),
-            Text("Inspiration"),
-            Text("Discover"),
-          ]),
-          TabBarView(children: [
-            Text("data1"),
-            Text("data2"),
-            Text("dat3"),
-          ])
+          Container(
+            child: TabBar(
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: [
+                  Tab(text: "Places"),
+                  Tab(text: "Inspirations"),
+                  Tab(text: "Discover"),
+                ]),
+          ),
+          Container(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(controller: _tabController, children: [
+              Text("data1"),
+              Text("data2"),
+              Text("dat3"),
+            ]),
+          )
         ],
       ),
     );
