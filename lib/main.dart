6bb4e9df2f86e4a41bac07b75424a72202481b94
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-20 18:01:25 +0500

    Detail Page Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:travelapp/details.dart';
+import 'package:travelapp/pages/main_page.dart';
+import 'package:travelapp/routes/routes.dart';
 
 import 'WelcomePage.dart';
 
@@ -18,6 +21,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        MyRoutes.welcome: (context) => WelcomePage(),
+        MyRoutes.mainpage: (context) => MainPage(),
+        MyRoutes.details: (context) => DetailsPage(),
+      },
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: WelcomePage(),
